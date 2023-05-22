87429c3c6e6a5e1b4defce2493ec5271281fd09e
jayjesture <oluwatobi1411@gmail.com>
2023-05-22 15:11:43 -0700

    3

---
+++ 0x01-variables_if_else_while/02-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 99; num1++)
+{
+for (num2 = 0; num2 <= 99; num2++)
+{
+if (num1 <= num2)
+{
+putchar('0' + num1 / 10);
+putchar('0' + num1 % 10);
+putchar(' ');
+putchar('0' + num2 / 10);
+putchar('0' + num2 % 10);
+if (num1 != 99 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return 0;
+}
