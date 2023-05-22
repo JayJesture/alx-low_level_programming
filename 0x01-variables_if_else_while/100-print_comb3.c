4a590fa8d7446bc653b104e1b795f6c680060c45
jayjesture <oluwatobi1411@gmail.com>
2023-05-22 13:39:35 -0700

    mand 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = '0'; i <= '9'; i++)
-{	
-	int m = i + 1;
-	for (; m <= '9'; m++)
-	{
-	    if (i == m)
-	    continue;
-	    putchar(i);
-	    putchar(m);
-	    if (!(i=='8' && m=='9'))
-	    {
-	    putchar(',');
-	    putchar(' ');
-	    }
-	}
-}
-putchar('\n');
-return (0);
+    int i;
+    for (i = '0'; i <= '9'; i++)
+    {
+    	int m = i + 1;
+    	
+    	for (; m <= '9'; m++)
+    	{
+    	    if (i == m)
+    	    continue;
+    	    putchar(i);
+    	    putchar(m);
+    	    if (!(i == '8' && m == '9'))
+    	    {
+    	        putchar(',');
+    	        putchar(' ');
+    	    }
+    	}
+    }
+    putchar('\n');
+    return (0);
 }
