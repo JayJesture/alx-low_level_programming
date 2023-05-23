8043be1e8c0fb37f0be045bc4cd01a8ff5d239c7
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-24 00:42:51 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 int i;
 if (n > 0)
 {
-_putchar('+');
+putchar('+');
 i = 1;
 }
 else if (n < 0)
 {
-_putchar('-');
+putchar('-');
 i = -1;
 }
 else
 {
-_putchar('0');
+putchar('0');
 i = 0;
 }
 return (i);
