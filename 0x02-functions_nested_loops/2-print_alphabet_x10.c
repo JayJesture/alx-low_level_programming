4b8dab5b9f3fedecbbb2dbd4ed82e09363329b09
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-24 00:05:47 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-#include std
+#include <stdio.h>
+/**
+ * main - print alpha
+ * Return: 0 if successful
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+for (; i < 10; i++)
+{
+char ch = 'a';
+for (; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+}  
+return (0);
+}
