3be2126aa3360640eaa0e529bc308d10b570dde8
jayjesture <oluwatobi1411@gmail.com>
2023-05-23 15:44:59 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print alpha
+ * Return: 0 if successful
+ */
+void main(void)
+{
+for (char ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H_
 
 int putchar(char c);
+void print_alphabet(void);
 
 #endif
