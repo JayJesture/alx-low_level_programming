3ecdb91e2e97eeb7b674e2becfebc7e9508f5b1f
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-24 00:20:41 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - print alpha
+ * _islower - print whethre lower or not
+ * @c: variable to determine if input is lowercase
  * Return: 0 if successful
  */
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-  return (1);
-  return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+return (0);
 }
