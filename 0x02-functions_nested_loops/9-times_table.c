127969a341aee0dacb0b1c1b2c1e9199a247feb1
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-25 23:15:22 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,21 +13,21 @@ int times1 = ((row * column)/10);
 int times2 = ((row * column)%10);
 if (((row)*(column+1)) < 10)
 {
-_putchar(times1 - '0');
-_putchar(times2 - '0');
+_putchar(times1 + '0');
+_putchar(times2 + '0');
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
 else
 {
-_putchar(times1 - '0');
-_putchar(times2 - '0');
+_putchar(times1 + '0');
+_putchar(times2 + '0');
 _putchar(',');
 _putchar(' ');
 }
 }
-_putchar((9 * row) - '0');
+_putchar((9 * row) + '0');
 _putchar('\n');
 }
 return;
