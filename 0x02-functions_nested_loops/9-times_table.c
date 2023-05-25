83310baf85005689bafdfd6afc235c05d171e53c
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-25 23:16:56 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ for (row = 0; row <= 9; row++)
 {
 for (column = 0; column <= 8; column++)
 {
-int times1 = ((row * column)/10);
+int times1 = ((row * column));
 int times2 = ((row * column)%10);
 if (((row)*(column+1)) < 10)
 {
