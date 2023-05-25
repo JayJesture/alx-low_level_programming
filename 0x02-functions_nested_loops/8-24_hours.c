bb7176ce46275939f7ec2afa098497d7a0eccd56
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-25 17:01:17 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,13 @@ int hour1;
 int hour2;
 int min1;
 int min2;
-for (hour1 = 0; hour1 <= 2; hour1++)
+for (hour1 = '0'; hour1 <= '2'; hour1++)
 {
-for (hour2 = 0; hour2 <= 9; hour2++)
+for (hour2 = '0'; hour2 <= '9'; hour2++)
 {
-for (min1 = 0; min1 <= 5; min1++)
+for (min1 = '0'; min1 <= '5'; min1++)
 {
-for (min2 = 0; min2 <= 9; min2++)
+for (min2 = '0'; min2 <= '9'; min2++)
 {
 _putchar(hour1);
 _putchar(hour2);
