f4774c2af4fbd08e94241827f3b20e93a900d42d
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-26 14:56:59 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_to_98 - print all to 98
+ * @n: value
+ */
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n > 98; n--)
+{
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+}
+} 
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('98');
+_putchar(\n);
+}
