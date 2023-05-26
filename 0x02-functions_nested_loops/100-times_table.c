9fdd1239a7c861d225e972fe2208f5fc2dd9adef
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-26 16:39:59 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,6 @@ else
 {
 if (times1 > 0)
 _putchar(times1 + '0');
-if (times2 > 0)
 _putchar(times2 + '0');
 _putchar(times3 + '0');
 _putchar(',');
@@ -49,7 +48,7 @@ _putchar(' ');
 lst = n * row;
 if ((lst / 100) != 0)
 _putchar(lst / 100 + '0');
-if (((lst / 10) % 10) != 0)
+if (((lst / 10) % 10) != 0 && ((lst / 100) != 0))
 _putchar(((lst / 10) % 10) + '0');
 _putchar(lst % 10 + '0');
 _putchar('\n');
