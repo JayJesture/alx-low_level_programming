c4d3bdfef628e917cdbbeebecf5832dd03178fbc
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-05-26 16:59:06 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * print_times_table - run time-table of int
+ * putchrr - put , then ' ' n times 
  * @n: value brought
  */
+void putchrr(int n);
 void print_times_table(int n)
 {
 int row, column, lst, times1, times2, times3;
@@ -23,19 +25,14 @@ _putchar(times1 + '0');
 if (times2 > 0)
 _putchar(times2 + '0');
 _putchar(times3 + '0');
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
+putchrr(3);
 }
 else if ((((row) * (column + 1)) > 9) && (((row) * (column + 1)) < 100))
 {
 if (times2 > 0)
 _putchar(times2 + '0');
 _putchar(times3 + '0');
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
+putchrr(2);
 }
 else
 {
@@ -43,8 +40,7 @@ if (times1 > 0)
 _putchar(times1 + '0');
 _putchar(times2 + '0');
 _putchar(times3 + '0');
-_putchar(',');
-_putchar(' ');
+putchrr(1);
 }
 }
 lst = n * row;
@@ -54,6 +50,18 @@ if (!((((lst / 10) % 10) == 0) && ((lst / 100) == 0)))
 _putchar(((lst / 10) % 10) + '0');
 _putchar(lst % 10 + '0');
 _putchar('\n');
+}
+return;
+}
+
+void putchrr(int n)
+{
+int i;
+  
+_putchar(',');
+for (i = 0; i < n; i++)
+{
+  _putchar(' ');
 }
 return;
 }
