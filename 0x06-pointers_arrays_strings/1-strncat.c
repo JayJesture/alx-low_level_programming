684edee6b13f63eef5731e908b36fb23c3d7e4fa
Odunayo John <104260413+JayJesture@users.noreply.github.com>
2023-06-03 02:49:58 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenates  strings taking n bytes from src
+ * @dest: first string
+ * @src: second string 
+ * @n: bites to use from src
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	for (; *temp != '\0'; temp++)
+		;
+	for (; *src != '\0'; src++)
+	{
+		if (n == 0)
+			break;
+		*temp = *src;
+		temp++;
+		n--;
+	}
+	return (dest);
+}
